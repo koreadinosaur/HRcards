58eabbfb9f06a14c61606366e8ad8014ade9497e
HyeongJin Kim <khj7741@gmail.com>
2022-10-16 21:10:47 +0900

    Home page header background, logo, text

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./home.module.css";
+
+import logo from "../../img/logo.png";
+const Home = ({ logout }) => {
+  return (
+    <div>
+      <header className={styles.home__header}>
+        <div className={styles.logo_box}>
+          <img src={logo} alt="hrcards" className={styles.logo} />
+        </div>
+        <div className={styles.text_box}>
+          <h1 className={styles.heading_primary}>
+            <span className={styles.heading_primary_main}>concentrate</span>
+            <span className={styles.heading_primary_sub}>
+              your own core business
+            </span>
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
